d8d3cd937f2a89ccdf550b3cb7ecfde602032925
anzhu <anzhu@cnord.ru>
2014-02-24 14:34:10 +0400

    Свежий PDF с работой Ольги
    - изменил размер картинок (оригинальные оставил)
    - подправил скрипт для сборки.

--- make-quickstart.sh
+++ make-quickstart.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 
 mv readme.md readme.mv
-mv 01-marketing-info.md 01-marketing-info.mv
 
 pandoc --template template/template-manual.latex -V lang=russian -o VR-QuickStart-Rus.pdf *.md
 
 mv readme.mv readme.md
-mv 01-marketing-info.mv 01-marketing-info.md
